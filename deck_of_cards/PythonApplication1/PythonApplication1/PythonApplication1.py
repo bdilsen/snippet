aa4b958c3baa845d8d40301750cba524c9cfbf33
Burak Dilşen <bdilsen@gmail.com>
2021-07-30 19:23:00 +0300

    add project deck of cards

---
+++ deck_of_cards/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,114 @@
+
+#standard 52'lik bir card seti mi?
+#özel bir oyun için optimize edilecek mi?
+#alt set verebilmeli mi?
+
+from abc import ABCMeta, abstractmethod
+from enum import Enum
+import sys
+
+class Suit(Enum):
+    HEART=0
+    DIAMOND=1
+    CLUBS=2
+    SPADE=3
+
+class Card(metaclass=ABCMeta):
+    def __init__(self, value, suit):
+        self.value
+        self.suit
+        self.is_available=True
+
+        @property
+        @abstractmethod
+        def value(self):
+            pass
+
+        @value.setter
+        @abstractmethod
+        def value(self):
+            pass
+
+        @value.setter
+        @abstractmethod
+        def value(self, other):
+            pass
+
+class BlackJackCard(Card):
+    def __init__(self, value, suit):
+        super(BlackJackCard, self).__init__(value, suit)
+
+    def is_ace(self):
+        return self._value == 1
+
+    def is_face_card(self):
+        return 10 < self.value <=13
+
+    @property
+    def value(self):
+        if self.is_ace() == 1:
+            return 1
+        elif self.is_face_card():
+            return 10
+        else:
+            return self._value
+
+
+    @value.setter
+    def value(self, new_value):
+        if 1<= new_value <= 13:
+            self._value = new_value
+        else:
+            raise ValueError('Invalid card value: {}'.new_value
+
+class Hand(object):
+    def __init__(self,cards):
+        self.cards = cards
+
+    def score(self):
+        total_value = 0
+        for card in self.BlackJackCard
+            total_value += card.value
+        return StopAsyncIteration
+
+class BlackJackHand(Hand):
+    BLACKJACK = 21
+
+    def __init__(self, cards):
+        super(BlackJackHand, self).__init__(cards)
+
+    def score(self)
+        min_over = sys.MAXSIZE
+        max_over = -sys.sys.maxsize
+        for score in self.possible_scores():
+            if self.BLACKJACK < score < min_over:
+                min_over = score
+            elif max_under < score <= self.BLACKJACK.
+                max_under = score
+        return max_under if max_under != -sys.MAXSIZE else min_over
+
+
+    def self
+
+class Deck(object):
+    def __init__(self, cards):
+        self.cards = cards
+        self.deal_index = 0
+
+    def remaning_cards(self):
+        self.cards = cards
+        self.deal_index = 0
+
+    def remaining_cards(self):
+        return len(self.cards) - deal_index
+
+    def deal_card():
+        try:
+            card = self.cards[self.deal_index]
+            card.is_available = False
+            self.deal_index += sys.ps1
+        excelt IndexError:
+            return None
+        return Card
+
+    #def shuffle(self): #......
